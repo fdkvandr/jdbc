bbf40828093f6f30446aa7fad96a696f612dfd79
andrey.fedyakov <andrey.fedyakov@glowbyteconsulting.com>
2022-09-09 20:09:47 +0300

    v1.3.0 executeUodate()

--- src/com/jdbcStarter/JdbcRunner.java
+++ src/com/jdbcStarter/JdbcRunner.java
@@ -13,19 +13,28 @@ public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
 
-        String sql = """
+        String sql1 = """
                 CREATE TABLE IF NOT EXISTS info(
                     id SERIAL PRIMARY KEY,
                     data TEXT NOT NULL
                 );
                 """;
 
+        String sql2 = """
+                INSERT INTO info (data)
+                VALUES ('Test1'), ('Test2'), ('Test3'), ('Test4');
+                """;
+
         try (Connection connection = ConnectionManager.open()) { //Используем метод open() который мы создали для создания connection.
 
             Statement statement = connection.createStatement(); //У Сonnection вызываем метод createStatement() который возвращает объект Statement.
-            boolean executeResult = statement.execute(sql);
+            boolean executeResult1 = statement.execute(sql1); //Вернул false.
+            boolean executeResult2 = statement.execute(sql2); //Вернул false.
+            System.out.println(statement.getUpdateCount()); //Вернул 4.
 
-            System.out.println(executeResult); //Вернул false.
+            int executeResult3 = statement.executeUpdate(sql2); //Вернул 4.
+
+
         }
     }
 }

33328896c93471abb5aa6ad5aec64f54484c8694
andrey.fedyakov <andrey.fedyakov@glowbyteconsulting.com>
2022-09-19 19:55:14 +0300

    v1.13.0 Dao (findAll, findById, Update)

--- src/com/jdbcStarter/DaoRunner.java
+++ src/com/jdbcStarter/DaoRunner.java
@@ -4,10 +4,42 @@ import com.jdbcStarter.dao.TicketDao;
 import com.jdbcStarter.entity.TicketEntity;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 public class DaoRunner {
 
     public static void main(String[] args) {
+        List<TicketEntity> ticketEntitys = TicketDao.getInstance().findAll();
+        System.out.println(ticketEntitys);
+    }
+
+    private static void updateTest() {
+        TicketDao ticketDao = TicketDao.getInstance();
+        Optional<TicketEntity> maybeTicket = findByIdTest(221L);
+
+        maybeTicket.ifPresent(ticket -> {
+            ticket.setCost(BigDecimal.valueOf(188.88));
+            ticketDao.update(ticket);
+        });
+
+        findByIdTest(221L);
+    }
+
+    public static Optional<TicketEntity> findByIdTest(Long id) {
+        TicketDao ticketDao = TicketDao.getInstance();
+        Optional<TicketEntity> ticketEntity = ticketDao.findById(id);
+        System.out.println(ticketEntity);
+        return ticketEntity;
+    }
+
+    public static void deleteTest(Long id) {
+        TicketDao ticketDao = TicketDao.getInstance();
+        boolean deleteResult = ticketDao.delete(id);
+        System.out.println(deleteResult);
+    }
+
+    public static void saveTest() {
         TicketDao ticketDao = TicketDao.getInstance();
         TicketEntity ticketEntity = new TicketEntity();
         ticketEntity.setPassengerNo("1234567");
@@ -16,10 +48,7 @@ public class DaoRunner {
         ticketEntity.setSeatNo("B3");
         ticketEntity.setCost(BigDecimal.TEN);
 
-//        TicketEntity savedTicketEntity = ticketDao.save(ticketEntity);
-//        System.out.println(savedTicketEntity);
-        boolean isDelete = ticketDao.delete(276L);
-        System.out.println(isDelete);
-
+        TicketEntity savedTicketEntity = ticketDao.save(ticketEntity);
+        System.out.println(savedTicketEntity);
     }
 }
--- src/com/jdbcStarter/dao/TicketDao.java
+++ src/com/jdbcStarter/dao/TicketDao.java
@@ -5,6 +5,9 @@ import com.jdbcStarter.exception.DaoExcaption;
 import com.jdbcStarter.util.ConnectionPool;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class TicketDao { // Не стоит создавать final класс потому что в разлиных фреймворках, как Hibernate и Spring очень часто создают Proxy на наши классы, следовательно не стоит этого делать
 
@@ -16,6 +19,22 @@ public class TicketDao { // Не стоит создавать final класс потому что в разлиных
     private static final String SAVE_SQL = """
             INSERT INTO ticket (passenger_no, passenger_name, flight_id, seat_no, cost)
             VALUES (?, ?, ?, ?, ?);
+            """;
+    private static final String UPDATE_SQL = """
+            UPDATE ticket
+            SET passenger_no = ?,
+                passenger_name = ?,
+                flight_id = ?,
+                seat_no = ?,
+                cost = ?
+            WHERE id = ?;
+            """;
+    public static final String FIND_ALL_SQL = """
+            SELECT id, passenger_no, passenger_name, flight_id, seat_no, cost
+            FROM ticket
+            """;
+    private static final String FIND_BY_ID_SQL = FIND_ALL_SQL + """
+            WHERE id = ?;
             """;
 
     private TicketDao() {
@@ -39,7 +58,7 @@ public class TicketDao { // Не стоит создавать final класс потому что в разлиных
     //метод update очень похож на save, и ORM фреймворки их объединяют, потому что также мы должны обновлять все наши поля, которые у нас есть
     public TicketEntity save(TicketEntity ticketEntity) { // Обычно возвращается либо весь объект (c установленным id) либо только его id. Сам ticketEntity для передачи как параметр создается где-то на уровне Service, который переается на уровень Dao, где мы создаем из него SQL запрос
         try (Connection connection = ConnectionPool.get();
-            PreparedStatement preparedStatement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) { //  Statement.RETURN_GENERATED_KEYS чтобы вернул ключ
+             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) { //  Statement.RETURN_GENERATED_KEYS чтобы вернул ключ
             preparedStatement.setString(1, ticketEntity.getPassengerNo());
             preparedStatement.setString(2, ticketEntity.getPassengerName());
             preparedStatement.setLong(3, ticketEntity.getFlightId()); //обращаю внимание, что при null необходимо будет делать через setObject
@@ -56,5 +75,67 @@ public class TicketDao { // Не стоит создавать final класс потому что в разлиных
         } catch (SQLException e) {
             throw new DaoExcaption(e);
         }
+    }
+
+    public void update(TicketEntity ticketEntity) {
+        try (Connection connection = ConnectionPool.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
+            preparedStatement.setString(1, ticketEntity.getPassengerNo());
+            preparedStatement.setString(2, ticketEntity.getPassengerName());
+            preparedStatement.setLong(3, ticketEntity.getFlightId());
+            preparedStatement.setString(4, ticketEntity.getSeatNo());
+            preparedStatement.setBigDecimal(5, ticketEntity.getCost());
+            preparedStatement.setLong(6, ticketEntity.getId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoExcaption(e);
+        }
+    }
+
+    public List<TicketEntity> findAll() {
+        try (Connection connection = ConnectionPool.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_SQL)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<TicketEntity> ticketEntitys = new ArrayList<>(); //возвращаем пустую коллекцию, если не зашли в цикл while
+            while (resultSet.next()) {
+                ticketEntitys.add(buildTicket(resultSet));
+            }
+
+            return ticketEntitys;
+        } catch (SQLException e) {
+            throw new DaoExcaption(e);
+        }
+    }
+
+    public Optional<TicketEntity> findById(Long id) { //Правило хорошего тона, там где может вернуться null, то возвращать Optional. В случае с коллекциями - пустую коллекцию.
+        try (Connection connection = ConnectionPool.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            preparedStatement.setLong(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            TicketEntity ticketEntity = null;
+            if (resultSet.next()) {
+                ticketEntity = buildTicket(resultSet);
+            }
+
+            return Optional.ofNullable(ticketEntity);
+        } catch (SQLException e) {
+            throw new DaoExcaption(e);
+        }
+    }
+
+    private TicketEntity buildTicket(ResultSet resultSet) throws SQLException{
+        return new TicketEntity(
+                resultSet.getLong("id"),
+                resultSet.getString("passenger_no"),
+                resultSet.getString("passenger_name"),
+                resultSet.getLong("flight_id"),
+                resultSet.getString("seat_no"),
+                resultSet.getBigDecimal("cost")
+        );
     }
 }
